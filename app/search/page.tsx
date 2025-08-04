350edc7208d44c85c3020401973030ea73893460
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-08-05 02:55:44 +0545

    completed spotify clone

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,27 +6,27 @@ import SearchContent from './components/SearchContent'
 
 export const revalidate = 0
 
-const Search = async ({
-    searchParams,
-}: {
-    searchParams?: Record<string, string | string[] | undefined>
-}) => {
-    const title = typeof searchParams?.title === 'string' ? searchParams.title : '';
-    const songs = await getSongsByTitle(title);
+interface SearchProps {
+  searchParams?: {
+    title?: string | string[];
+  };
+}
 
-    return (
-        <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
-            <Header className='from-bg-neutral-900'>
-                <div className='mb-2 flex flex-col gap-y-6'>
-                    <h1 className='text-white text-3xl font-semibold'>
-                        Search
-                    </h1>
-                    <SearchInput />
-                </div>
-            </Header>
-            <SearchContent songs={songs} />
+const Search = async ({ searchParams }: SearchProps) => {
+  const title = typeof searchParams?.title === "string" ? searchParams.title : ""
+  const songs = await getSongsByTitle(title)
+
+  return (
+    <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+      <Header className='from-bg-neutral-900'>
+        <div className='mb-2 flex flex-col gap-y-6'>
+          <h1 className='text-white text-3xl font-semibold'>Search</h1>
+          <SearchInput />
         </div>
-    )
+      </Header>
+      <SearchContent songs={songs} />
+    </div>
+  )
 }
 
 export default Search
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,14 @@
-// eslint.config.js
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-empty-object-type": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    }
-  }
-];
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+]
+ 
+export default eslintConfig
